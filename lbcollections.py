25f8c6822afa31c903cbc862b04eaa404610668e
khmkii <james_mccombie@hotmail.com>
2018-04-07 12:11:01 +0100

    Edit insert method functionality completed

--- lbcollections.py
+++ lbcollections.py
@@ -3,7 +3,6 @@ class Element:
         self.value = value
         self.next = None
         self.root = False
-        # would it be an idea to store the position for each element
 
 
 class LinkedList:
@@ -12,8 +11,10 @@ class LinkedList:
         self.root_element.root = True
 
     def append(self, add_value):
-        """adds a new link to the end of the linked list, takes a value
-        parameter named add_value"""
+        """
+        adds a new link to the end of the linked list, takes a value
+        parameter named add_value
+        """
 
         current = self.root_element
         while current.next:
@@ -28,7 +29,7 @@ class LinkedList:
                     current = current.next
                     if current is None:
                         return None
-                return current 
+                return current
             else:
                 raise ValueError(
                     "position must be an integer greater than zero"
@@ -36,10 +37,29 @@ class LinkedList:
         else:
             raise TypeError("position must be an integer")
 
-    def insert(self, new_element, position):
-        pass
+    def insert(self, new_element_value, position):
+        """
+        Insert a new node at the given position.
+        Assume the first position is "1".
+        Inserting at position 3 means between
+        the 2nd and 3rd elements.
+        """
+
+        if position == 1:
+            self.root_element.root = False
+            self.root_element = Element(new_element_value)
+            self.root_element.root = True
+        else:
+            posterior = self.get_position(position)
+            if posterior is not None:
+                new_element = Element(new_element_value)
+                prior = self.get_position(position-1)
+                new_element.next = posterior
+                prior.next = new_element
+            else:
+                raise IndexError(
+                        "cannot insert outside of the linked list, try append instead"
+                    )
 
     def delete(self, value):
         pass
-
-    # should have a str function that displays the values in the list
